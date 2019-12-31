a6c508e80b838c0dcb4dc5e28bfae1254dba017a
fanofFOSS <41705411+fanofFOSS@users.noreply.github.com>
2019-12-31 15:12:18 +0530

    updating 2 to hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 
 public static void main(String args[]){
-System.out.println("Hello, "+args[0]);
-System.out.println("Hope you are having agood day, "+args[0]);
+// System.out.println("Hello, "+args[0]);
+System.out.println("Hope you are having a good day, "+args[0]);
 }
 }
